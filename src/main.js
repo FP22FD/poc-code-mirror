f75e28448b7bd30a4e4e55bcdf15bcec00a30bdd
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-06-04 21:47:47 +0200

    Add themes

--- src/main.js
+++ src/main.js
@@ -3,7 +3,20 @@ import { javascript } from '@codemirror/lang-javascript'
 import { EditorState } from '@codemirror/state'
 import { EditorView } from '@codemirror/view'
 import { basicSetup } from 'codemirror'
+
+/* Themes */
 import { oneDarkTheme } from "@codemirror/theme-one-dark"
+import { materialLight } from '@ddietr/codemirror-themes/material-light'
+import { materialDark } from '@ddietr/codemirror-themes/material-dark'
+import { solarizedLight } from '@ddietr/codemirror-themes/solarized-light'
+import { solarizedDark } from '@ddietr/codemirror-themes/solarized-dark'
+import { dracula } from '@ddietr/codemirror-themes/dracula'
+import { githubLight } from '@ddietr/codemirror-themes/github-light'
+import { githubDark } from '@ddietr/codemirror-themes/github-dark'
+import { aura } from '@ddietr/codemirror-themes/aura'
+import { tokyoNight } from '@ddietr/codemirror-themes/tokyo-night'
+import { tokyoNightStorm } from '@ddietr/codemirror-themes/tokyo-night-storm'
+import { tokyoNightDay } from '@ddietr/codemirror-themes/tokyo-night-day'
 
 import "/style.css";
 
@@ -17,7 +30,14 @@ new EditorView({
     extensions: [
       basicSetup,
       javascript(),
-      oneDarkTheme
+      // oneDarkTheme
+      // githubDark
+      // materialDark
+      // solarizedDark
+      dracula
+      // aura
+      // tokyoNight
+      // tokyoNightStorm
     ],
   }),
 })
